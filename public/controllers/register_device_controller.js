659b61e844310c9cb9fe8da8d70e6590a73f2f97
YianniR <yiannis.rav@gmail.com>
2018-01-10 22:57:03 +0000

    Added error pop ups for invalid data and update of IMEI data

--- public/controllers/register_device_controller.js
+++ public/controllers/register_device_controller.js
@@ -5,22 +5,51 @@ myApp.controller('register_device_controller', ['$scope', '$http', '$location', 
 
   $scope.postDevice = function(){
     document.getElementById('register-button').onclick = function () {
-      var data;
-      data={
-        device_name: document.getElementById('device-name').value,
-        imei: document.getElementById('IMEI').value,
-        datatype1: document.getElementById('datatype1').value,
-        datatype2: document.getElementById('datatype2').value,
-        datatype3: document.getElementById('datatype3').value
+      $http.get('/api_registered_devices').then(function(responce){
+        registered_devices_data = responce.data;
+        var imei_list = new Array();
 
-      };
-      console.log(data)
-      $http.post('/api_registered_devices',data).then(function(responce){
-        $scope.temps = responce.data;
+        for (i = 0; i<registered_devices_data.length; i++ ){
+          imei_list.push(registered_devices_data[i].imei);;
+        };
+
+        if(imei_list.includes(document.getElementById('IMEI').value)==true){
+          if (confirm("This IMEI is already registered! Do you want to update its details?") == true) {
+            var data;
+            data={
+              device_name: document.getElementById('device-name').value,
+              imei: document.getElementById('IMEI').value,
+              datatype1: document.getElementById('datatype1').value,
+              datatype2: document.getElementById('datatype2').value,
+              datatype3: document.getElementById('datatype3').value
+            };
+
+            $http.post('/api_registered_devices',data).then(function(responce){
+              $scope.temps = responce.data;
+            });
+
+            location.href = "#!/register_success";
+          }
+          else{
+            location.href = '#!/'
+          }
+        } 
+        else {
+          var data;
+          data={
+            device_name: document.getElementById('device-name').value,
+            imei: document.getElementById('IMEI').value,
+            datatype1: document.getElementById('datatype1').value,
+            datatype2: document.getElementById('datatype2').value,
+            datatype3: document.getElementById('datatype3').value
+          };
+
+          $http.post('/api_registered_devices',data).then(function(responce){
+            $scope.temps = responce.data;
+          });
+          location.href = "#!/register_success";
+        }
       });
-
-      location.href = "#!/register_success";
-
     }
   }
 
--- public/controllers/temp_selected_controller.js
+++ public/controllers/temp_selected_controller.js
@@ -7,8 +7,26 @@
       document.getElementById('search-button').onclick = function () {
       search_date=document.getElementById('selected-date').value;
       imei=document.getElementById('imei').value;
-      console.log(imei);
-      location.href = "#!/tempfigure/"+imei+"/"+search_date;
+
+      $http.get('/api_registered_devices').then(function(responce){
+        registered_devices_data = responce.data;
+        var imei_list = new Array();
+
+        for (i = 0; i<registered_devices_data.length; i++ ){
+          imei_list.push(registered_devices_data[i].imei);;
+        };
+
+        if(imei_list.includes(imei)!=true){
+          alert("This IMEI is not registered!");
+        }
+        else if(search_date.length<10){
+          alert("Please input the date in the format specified!");
+          location.href = '#!/';
+        }
+        else{
+          location.href = "#!/tempfigure/"+imei+"/"+search_date;
+        }
+      });
     };
   };
 }])
